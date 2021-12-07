9a787e777d52ef9b0e725a311e35248de485f37c
LeonGr <mail+github@leongr.nl>
2021-12-07 07:29:04 +0100

    improve performance

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -6,42 +6,36 @@ fn read_line_to_ints() -> Vec<i32> {
         .collect()
 }
 
-fn part1(input: &Vec<i32>) -> i32 {
-    let mut min_cost = i32::MAX;
+fn solve(input: &Vec<i32>) {
+    let mut min_cost_1 = i32::MAX;
+    let mut min_cost_2 = i32::MAX;
 
     for i in 0..=(*input.iter().max().unwrap()) {
-        let mut cost = 0;
+        let mut cost_1 = 0;
+        let mut cost_2 = 0;
+
         for num in input {
-            cost += (num - i).abs()
-        }
-        if cost < min_cost {
-            min_cost = cost;
-        }
-    }
+            cost_1 += (num - i).abs();
 
-    min_cost
-}
-
-fn part2(input: &Vec<i32>) -> i32 {
-    let mut min_cost = i32::MAX;
-
-    for i in 0..=(*input.iter().max().unwrap()) {
-        let mut cost = 0;
-        for num in input {
             let dist = (num - i).abs();
-            cost += (dist * (dist + 1)) / 2;
+            cost_2 += (dist * (dist + 1)) / 2;
         }
-        if cost < min_cost {
-            min_cost = cost;
+
+        if cost_1 < min_cost_1 {
+            min_cost_1 = cost_1;
+        }
+
+        if cost_2 < min_cost_2 {
+            min_cost_2 = cost_2;
         }
     }
 
-    min_cost
+    println!("Part 1: {}", min_cost_1);
+    println!("Part 2: {}", min_cost_2);
 }
 
 fn main() {
     let input = read_line_to_ints();
 
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    solve(&input);
 }
